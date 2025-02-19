6eb17d11cf1558b8738da20c4a4c7c5dbc3ab103
Ardyezapratama <ardyezapratama@gmail.com>
2025-02-19 10:25:48 +0800

    Challenge 3

--- challenge.js
+++ challenge.js
@@ -76,3 +76,22 @@ const calcHumanYears = function (dogAges) {
 calcHumanYears([5, 2, 4, 1, 15, 8, 3]);
 calcHumanYears([16, 6, 10, 5, 6, 1, 4]);
 */
+
+// NOTE: Challenge 3
+/*
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+/*
+const calcAverageHumanAge = (ages) =>
+	ages
+		.map((age) => (age <= 2 ? age * 2 : 16 + age * 4))
+		.filter((age) => age >= 18)
+		.reduce((acc, age, idx, arr) => acc + age / arr.length, 0);
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+*/
